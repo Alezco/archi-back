d8ccf4939aaa638aed944214034e3fc4749e8e75
paultrh <paultr_h@epita.fr>
2017-12-17 23:16:37 +0100

    Update ping.py

--- src/ping.py
+++ src/ping.py
@@ -3,10 +3,15 @@ import os
 import time
 import Constants
 
-for i in range(0, 100):
+for i in range(0, 10000):
     time.sleep(5)
     result = subprocess.Popen(["ping","-c", "1", Constants.HOST]).wait()
     if not result:
         print("active")
     else:
         print("inactive")
+        result2 = subprocess.Popen(['/home/ec2-user/passRoMaster'], shell=True, executable="/bin/bash").wait()
+        if not result2:
+            print("failed to move elasticIP")
+        else:
+            print("elasticIP moved")
